88723a1acb5ae375ebac864cb47274d8e02a4ece
marek <marekdanco.mail@gmail.com>
2025-10-13 14:07:52 +0200

    update script

--- config.sh
+++ config.sh
@@ -3,31 +3,32 @@ set -e
 
 echo "Setting up dotfiles"
 
-# Helper function to remove a file or symlink if it exists
-remove() {
+# Helper function: backup a file/folder if it exists
+backup_and_remove() {
     local target="$1"
     if [ -e "$target" ] || [ -L "$target" ]; then
-        echo "Removing existing $target"
-        rm -rf "$target"
+        local backup="${target}.bak_$(date +%Y%m%d%H%M%S)"
+        echo "Backing up $target to $backup"
+        mv "$target" "$backup"
     fi
 }
 
 # Vim
-remove "$HOME/.vimrc"
-remove "$HOME/.vim"
+backup_and_remove "$HOME/.vimrc"
+backup_and_remove "$HOME/.vim"
 ln -s "$HOME/dotfiles/vim/.vimrc" "$HOME/.vimrc"
 ln -s "$HOME/dotfiles/vim/.vim" "$HOME/.vim"
 echo "Vim configured."
 
 # Kitty
-remove "$HOME/.config/kitty"
+backup_and_remove "$HOME/.config/kitty"
 mkdir -p "$HOME/.config"
 ln -s "$HOME/dotfiles/kitty" "$HOME/.config/kitty"
 echo "Kitty configured."
 
 # Bash
-remove "$HOME/.bashrc"
-remove "$HOME/.bash_aliases"
+backup_and_remove "$HOME/.bashrc"
+backup_and_remove "$HOME/.bash_aliases"
 ln -s "$HOME/dotfiles/bash/.bashrc" "$HOME/.bashrc"
 ln -s "$HOME/dotfiles/bash/.bash_aliases" "$HOME/.bash_aliases"
 echo "Bash configured."
