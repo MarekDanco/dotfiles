0c52a847767410ddfa353680b65fd1727c8c259f
marek <marekdanco.mail@gmail.com>
2025-10-25 11:16:43 +0200

    oil

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -1,5 +1,17 @@
 return {
   {
+    'stevearc/oil.nvim',
+    dependencies = vim.g.have_nerd_font and { 'nvim-tree/nvim-web-devicons' } or { 'nvim-mini/mini.icons', opts = {} },
+    opts = {
+      default_file_explorer = true,
+    },
+    lazy = false,
+    config = function(opts)
+      require('oil').setup(opts)
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory with Oil' })
+    end,
+  },
+  {
     'bohlender/vim-smt2',
     ft = 'smt2',
   },
