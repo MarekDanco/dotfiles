27a42f60810ff2f35d2132dce0f04f840c6b041c
marek <marekdanco.mail@gmail.com>
2025-10-25 22:57:45 +0200

    change treesitter branch

--- nvim/init.lua
+++ nvim/init.lua
@@ -496,7 +496,7 @@ require('lazy').setup({
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
+    branch = 'main',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       auto_install = true,
